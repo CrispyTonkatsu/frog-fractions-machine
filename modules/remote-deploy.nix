6278e33e92a55f27252e774347474e3ca42c2916
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-04-29 01:42:59 -0700

    Added notes

--- modules/remote-deploy.nix
+++ modules/remote-deploy.nix
@@ -1,38 +1,7 @@
 { ... }:
 {
+  # NOTE: Add the other member's keys here so they can also remote deploy
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZpwDM9bJ299cwFmS9AvGolTNcOHSaLcewTQ0zEbupF erina@nixos"
   ];
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-    };
-  };
-
-  networking = {
-    interfaces.thunderbolt0.ipv4.addresses = [
-      {
-        address = "10.0.0.1";
-        prefixLength = 24;
-      }
-    ];
-  };
-
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      workstation = true;
-    };
-  };
-
-  # TODO: Get this working
-  systemd.network.networks."40-thunderbolt" = {
-    matchConfig.Name = "thunderbolt0";
-    linkConfig.RequiredForOnline = "no";
-  };
 }
